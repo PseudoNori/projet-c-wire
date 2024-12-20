a1e8e576d6e30ca1fca535e2a6b848c55e68a5a4
matheo m <166307348+braysake@users.noreply.github.com>
2024-12-20 19:28:50 +0100

    Update main.c

--- Code_C/main.c
+++ Code_C/main.c
@@ -4,13 +4,13 @@ int main(int argc,char *argv[]){
     FILE* input=NULL;
     FILE* output=NULL;
     pTree AVL=NULL;
-	if(argc!=3){
-		printf("error wrong argument \n");
-		exit(5);
-	}
+    if(argc!=3){
+	printf("error wrong argument \n");
+	exit(5);
+    }
 	
-	int verif=1;
-	input=fopen(argv[1], "a");
+    int verif=1;
+    input=fopen(argv[1], "a");
     if(input==NULL){
         printf("%s\n",argv[1]);
         printf("error fopen \n");
@@ -19,8 +19,6 @@ int main(int argc,char *argv[]){
     if(ftell(input)==0){
     	verif=0;
     }
-    
-    
     fclose(input);
     
     input=fopen(argv[1], "r");
@@ -29,7 +27,7 @@ int main(int argc,char *argv[]){
         printf("error fopen \n");
         exit(2);
     }
-	if(verif){
+    if(verif){
     	AVL=extract(input);  //argv1= nom du fichier
     }
 	fclose(input);
