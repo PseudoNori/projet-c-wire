ea4addfd0bcc611d07abad450efc90adf08468ba
PseudoNori <166307840+PseudoNori@users.noreply.github.com>
2024-12-20 10:13:12 +0100

    Update executable.c

--- Code_C/executable.c
+++ Code_C/executable.c
@@ -2,49 +2,26 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-#include<unistd.h>
+
 typedef struct AVLcsv{
     //int type;    // 1=HV_B 2=HV_A 3=LV
-    long double id; // num id
-    long double capacity;
-    long double consomation;
+    int id; // id number
+    int height; 
     struct AVLcsv* filsG;
     struct AVLcsv* filsD;
-    int height;
+	long double capacity;
+    long double consomation;
 }AVLcsv;
-typedef AVLcsv* pArbre;
 
-/*void linebreak(FILE* fichier){
-    while(*fichier != '\n'){
-        fgetc(fichier);
-    }
-}
-*/
-/*
-void nextsemicolon(FILE* fichier){
-    //fgetc(fichier);
-    while(fgetc(fichier)!=';'){
-       fgetc(fichier);
-    }
-}*/
+typedef AVLcsv* pTree; // pointer on AVLcsv are now pTree
 
-/*int extractvalue(FILE* fichier, int cap){
-    fscanf(fichier, %d, cap);
-    return cap;
-}
-*/
-int height(pArbre a){
-    if (a == NULL)
-        return 0;
-    return a->height;
-}
 
-pArbre addAVL(FILE* flux, pArbre a){
+pTree addAVL(FILE* flux, pTree a){
     long double capa, cons;
     char tab[100];
 
     if(fscanf(flux,";%Lf;%Lf",&capa,&cons)==-1){
-    	printf("probleme fscanf");
+    	printf("error fscanf");
     	exit(3);
     }
 
@@ -54,62 +31,54 @@ pArbre addAVL(FILE* flux, pArbre a){
     return a;
 }
 
-pArbre crAVL(long double i){
-    pArbre a;
+pTree crAVL(int i){
+    pTree a;
     a=malloc(sizeof(AVLcsv));
     a->capacity=0;
-    /*if (a->capacity<=0){
-        exit(1);
-    }*/
-    /*a->type=t;
-    if (a->type<=0 || a->type>=4){
-        exit(1);
-    }*/
     a->id=i;
     if (a->id<=0){
-        exit(1);
+    	exit(1);
     }
     a->consomation=0;
-    /*if (a->consomation<=0){
-        exit(1);
-    } */
     a->filsG=NULL;
     a->filsD=NULL;
     a->height=1;
     return a;
 }
 
-pArbre rightRotate(pArbre a){
-    pArbre p = a->filsG;
-    pArbre c = p->filsD;
+int height(pTree a){
+    if (a == NULL)
+        return 0;
+    return a->height;
+}
 
+pTree rightRotate(pTree a){
+    pTree p = a->filsG;
+    pTree c = p->filsD;
     p->filsD = a;
     a->filsG = c;
-
     a->height = fmax(height(a->filsG),height(a->filsD)) + 1;
     p->height = fmax(height(p->filsG),height(p->filsD)) + 1;
     return p;
 }
 
-pArbre leftRotate(pArbre a){
-    pArbre p = a->filsD;
-    pArbre c = p->filsG;
-
+pTree leftRotate(pTree a){
+    pTree p = a->filsD;
+    pTree c = p->filsG;
     p->filsG = a;
     a->filsD = c;
-
     a->height = fmax(height(a->filsG),height(a->filsD)) + 1;
     p->height = fmax(height(p->filsG),height(p->filsD)) + 1;
     return p;
 }
 
-int getBalance(pArbre a){
+int getBalance(pTree a){
     if (a == NULL)
         return 0;
     return height(a->filsG) - height(a->filsD);
 }
 
-pArbre insertAVL(pArbre a, FILE* flux, long double i){
+pTree insertAVL(pTree a, FILE* flux, int i){
     if(a==NULL){
         a=crAVL(i);
         a=addAVL(flux, a);
@@ -129,7 +98,7 @@ pArbre insertAVL(pArbre a, FILE* flux, long double i){
                         height(a->filsD));
 
     int balance = getBalance(a);
-
+	// Rotations
     if (balance > 1 && i < a->filsG->id){
         return rightRotate(a);
     }
@@ -147,82 +116,34 @@ pArbre insertAVL(pArbre a, FILE* flux, long double i){
     }
     return a;
 }
-/*extract(FILE* entre, int nbcentrale, int t){
-    int cen, int t, int cap, char* typeID, char* typeIDtmp;
-    a=fgetc(fichier);
-    while(a!=nbcentrale){
-        linebreak(fichier);
-        a=fgetc(fichier);
-    }
-    while(a==nbcentrale || ){    
-        for(int i=0, i<t-1, i++){
-            nextsemicolon(fichier);
-        }
-        fscanf(fichier,%d, &typeID);
-        typeIDtmp=typeID;
-        fscanf(fichier, %lf, &cap)
-        linebreak(fichier);
-        while(typeIDtmp==typeID && ){
-            for(int j=0, j<t, j++){
-                nextsemicolon;                  
-            }
-            
-        }
-            case 1:
-                sort()
-                break;
-            case 2:
-                nextsemicolon(fichier);
-                sort;
-                break;
-            case 3:
-                nextsemicolon(fichier);
-                nextsemicolon(fichier);
-                sort;
-                break;
-        }
-    }
-}*/
 
-void parcour_infixe(pArbre a, FILE* flux){
+void infixe_print(pTree a, FILE* flux){
     if(a!=NULL && a->filsD==NULL && a->filsG==NULL){
-        fprintf(flux,"%.0Lf;%.0Lf;%.0Lf\n",a->id, a->capacity, a->consomation);
+        fprintf(flux,"%c:%.0Lf:%.0Lf\n",a->id, a->capacity, a->consomation);
     }
     else if((a!=NULL)){
-            parcour_infixe(a->filsG,flux);
-            fprintf(flux,"%.0Lf;%.0Lf;%.0Lf\n",a->id, a->capacity, a->consomation);
-            parcour_infixe(a->filsD,flux);
+            infixe_print(a->filsG,flux);
+            fprintf(flux,"%c:%.0Lf:%.0Lf\n",a->id, a->capacity, a->consomation);
+            infixe_print(a->filsD,flux);
     }
 }
 
-pArbre extract(FILE* flux,int nbcent, int type, int constype){
-    pArbre a=NULL;
-    long double i=0;
-
-    if(fscanf(flux,"%Lf",&i)==-1){
-    	printf("probleme fscanf\n");
-    	exit(3);
-    }
+pTree extract(FILE* flux){
+    pTree a=NULL;
+    char i=fgetc(flux);
     
    	do{
     	a=insertAVL(a,flux,i);
-        //printf("%d %Lf\n",ftell(flux),i);
-        if(fscanf(flux,"%Lf",&i)==-1){
-            i=fgetc(flux);
-        }
+    	//printf("%c \n",i);
+    	i=fgetc(flux);
 	}while(i!=EOF);
-/*
-	printf("%c %Lf %Lf \n",a->id, a->capacity, a->consomation);
-    printf("%c %Lf %Lf \n",a->filsG->id, a->filsG->capacity, a->filsG->consomation);
-	printf("%c %Lf %Lf \n",a->filsD->id, a->filsD->capacity, a->filsD->consomation);
-*/
     return a;
 }
 
 int main(int argc,char *argv[]){
     FILE* input=NULL;
     FILE* output=NULL;
-    pArbre AVL=NULL;
+    pTree avl=NULL;
 
     input=fopen(argv[1], "r");
     if(input==NULL){
@@ -230,16 +151,13 @@ int main(int argc,char *argv[]){
         printf("error fopen");
         exit(2);
     }
-    if(argv[2]<=0){
-    	exit(2);
-    }
-    AVL=extract(input,atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));  //argv1= nom du fichier argv2=num_centrale argv3=type argv4=consomateur
-
-    output=fopen("../tmp/res_c.dat", "w");
+    avl=extract(input);  //argv1= nom du fichier
+    output=fopen("../tmp/res_c.csv", "w");
     if(output==NULL){
         printf("error fopen res");
         exit(4);
     }
-    parcour_infixe(AVL,output);
+    infixe_print(avl,output);
 return 0;
 }
+
